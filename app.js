b8499e9665da0910e36ea5eaafb6c3e25238819d
git-user-bcn <david.monne@yahoo.es>
2015-06-23 15:57:33 +0100

    esqueleto express-generator v2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Se importan todos los paquetes necesarios, así como paquetes con Middlewares
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,51 +6,62 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Se importan también los enrutadores
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// Se crea la aplicación
 var app = express();
 
 // view engine setup
+// Path donde están las vistas y parámetro View Engine, generador de vistas EJS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+// Se instalan todos los Middlewares asociados anteriormente
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Se instalan los enrutadores que se importaron anteriormente
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
+// Middleware que procesará el resto de rutas que no sean las 2 de arriba (ruta vacía y ruta /users)
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
+    // Se invoca el siguiente Middleware de error
     next(err);
 });
 
-// error handlers
+// Error Handlers
 
-// development error handler
-// will print stacktrace
+// development error handler: will print stacktrace
+// Si estamos en la fase de desarrollo, se instalará el siguiente Middleware
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
+        // Se renderiza una pequeña vista mostrando el mensaje
         res.render('error', {
             message: err.message,
+            // El stack de errores contendrá mucha información sobre cualquier posible error
             error: err
         });
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// production error handler: no stacktraces leaked to user
+// Gestión de errores de producción. Realiza lo mismo que el anterior excepto por el stack de errores, que estará vacío
+// Por lo que el mensaje será mucho más simple
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
+    // Se renderiza una pequeña vista mostrando el mensaje
     res.render('error', {
         message: err.message,
         error: {}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+// Enrutador para la ruta vacía
 var express = require('express');
+// Se genera un enrutador básico en la variable router
 var router = express.Router();
 
 /* GET home page. */
+// Establecemos la ruta vacía en el router, que renderizará el fichero index
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+// Se exporta el enrutador con los Middlewares instalados
 module.exports = router;
