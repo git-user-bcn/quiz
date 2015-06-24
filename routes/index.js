3f5405acd7643774cb7b8e313f14e7565dd5f30d
git-user-bcn <david.monne@yahoo.es>
2015-06-24 17:22:24 +0100

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,18 @@
 var express = require('express');
 // Se genera un enrutador básico en la variable router
 var router = express.Router();
+// Se importa quiz_controller.js
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 // Establecemos la ruta vacía en el router, que renderizará el fichero index
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+// Introducimos dos nuevas rutas en el enrutador. Nos lleva a ejecutar las acciones asociadas a question y answer definidas en el controlador
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 // Se exporta el enrutador con los Middlewares instalados
 module.exports = router;
