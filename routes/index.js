f2ebeb6c72e6198b753561063f96f2034c6d8a1a
git-user-bcn <david.monne@yahoo.es>
2015-07-23 18:12:15 +0100

    Borrar preguntas

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,7 @@ router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
 // Introducimos una nueva ruta en el enrutador, para la vista del autor de la página
 router.get('/author', function(req, res) {
